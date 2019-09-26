03b62a12db299a7b5d2b9fcef16afe615378d829
btdvalle <btdvalle@gmail.com>
2019-09-26 14:14:42 +0200

    In progress

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,17 @@
 import React from "react";
 import Header from "./Header";
+import Home from "./Home";
+import { Route, Switch } from "react-router-dom";
 
 class App extends React.Component {
   render() {
     return (
       <div className="App">
         <Header />
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/about" />
+        </Switch>
       </div>
     );
   }
