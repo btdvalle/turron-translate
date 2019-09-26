8eb8e33cb0792d8c08c9ec7265051c0f2bdf757e
btdvalle <btdvalle@gmail.com>
2019-09-26 14:06:22 +0200

    Init project

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from "./Header";
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <Header />
+      </div>
+    );
+  }
+}
+
+export default App;
