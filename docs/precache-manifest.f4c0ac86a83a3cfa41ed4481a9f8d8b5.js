198a3be0a53afe186a2c00f0ed3fa641c0cf4d89
Mariasola <mariasolahornedo@gmail.com>
2019-09-27 14:54:41 +0200

    docs

---
+++ docs/precache-manifest.f4c0ac86a83a3cfa41ed4481a9f8d8b5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c5a126fea1f2d7536a8c374c4d7ed889",
+    "url": "./index.html"
+  },
+  {
+    "revision": "af0c2823dd2a8fb19650",
+    "url": "./static/css/main.57149206.chunk.css"
+  },
+  {
+    "revision": "dbf21dd6d6284f200518",
+    "url": "./static/js/2.0ee7e11a.chunk.js"
+  },
+  {
+    "revision": "af0c2823dd2a8fb19650",
+    "url": "./static/js/main.11cdbebf.chunk.js"
+  },
+  {
+    "revision": "1e019d54689f3becd15d",
+    "url": "./static/js/runtime-main.8b127a42.js"
+  },
+  {
+    "revision": "53790dabf51186ab2e2697cb82d916c9",
+    "url": "./static/media/hulk-miguel-icon.53790dab.png"
+  },
+  {
+    "revision": "4d67f82f0ac65035671f3f5380735ebe",
+    "url": "./static/media/promoGrace.4d67f82f.jpg"
+  }
+]);
